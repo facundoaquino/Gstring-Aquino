f4244f488944181842ea84774a835ac56d195c0f
Facundo Aquino <facun90@live.com>
2021-05-16 18:23:01 -0300

    firebase config initialized , get products from firebase

--- src/componets/container/ItemListContainer.js
+++ src/componets/container/ItemListContainer.js
@@ -4,18 +4,25 @@ import { getDataProducts } from '../../helpers/getDataProducts'
 import ItemList from './ItemList'
 import Loading from './Loading'
 import './styles/main.css'
+import db from './../../firebase/firebase'
 const ItemListContainer = () => {
 	const { categoryId } = useParams()
 	const [data, setData] = useState({ products: [], loading: true })
 	const { products, loading } = data
 	useEffect(() => {
-		getDataProducts().then((res) => setData({ products: res, loading: false }))
+		const items = db.collection('items')
+		items.get().then((snapShot) => {
+			const allItems = snapShot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+			console.log(allItems)
+			setData({ products: allItems, loading: false })
+		})
+		// getDataProducts().then((res) => setData({ products: res, loading: false }))
 	}, [])
 
-	useEffect(() => {
-		setData({ loading: true })
-		getDataProducts(categoryId).then((res) => setData({ products: res, loading: false }))
-	}, [categoryId])
+	// useEffect(() => {
+	// 	setData({ loading: true })
+	// 	getDataProducts(categoryId).then((res) => setData({ products: res, loading: false }))
+	// }, [categoryId])
 
 	return <main className="main__container">{loading ? <Loading /> : <ItemList products={products} />}</main>
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: 'AIzaSyD4SVnXMWKVa68HJgV3bYuH4RWALJnIUWQ',
+	authDomain: 'coderhouse-ecommerce-322cc.firebaseapp.com',
+	projectId: 'coderhouse-ecommerce-322cc',
+	storageBucket: 'coderhouse-ecommerce-322cc.appspot.com',
+	messagingSenderId: '955062718048',
+	appId: '1:955062718048:web:5644be19b46780f5b6fd05',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export default firebase.firestore()
